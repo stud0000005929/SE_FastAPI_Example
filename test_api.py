ed49388335cf1bbaab1eae030649e3c09533ae1f
Zhukov Dmitry <146352203+stud0000228135@users.noreply.github.com>
2024-02-29 20:01:02 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -6,4 +6,4 @@ client = TestClient(app)
 def test_translate_endpoint():
     response = client.post("/predict/", json={"text": "Hello, world!"})
     assert response.status_code == 200
-    assert response.json() == {'label': 'POSITIVE'}
+    assert response.json() == [{'label': 'POSITIVE', 'score': 0.9997164607048035}]

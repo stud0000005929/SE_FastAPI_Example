2a705357623b9c1bf383b8e8609026a3b04ca62c
Акимцев Алексей <125552026+ReVioletMist@users.noreply.github.com>
2024-02-29 18:44:40 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,30 +3,30 @@ from transformers import pipeline
 from pydantic import BaseModel
 
 class Item(BaseModel):
-text: str
+    text: str
 
 # Инициализация модели
 classifier = pipeline("sentiment-analysis")
 
 # Определение функции предсказания
 def predict(item: Item):
-return classifier(item.text)
+    return classifier(item.text)
 
 # Основное веб-приложение Streamlit
 def main():
-st.title('Sentiment Analysis with Streamlit')
+    st.title('Sentiment Analysis with Streamlit')
 
-# Поле ввода текста
-text_input = st.text_area("Enter text:", "")
+    # Поле ввода текста
+    text_input = st.text_area("Enter text:", "")
 
-# Кнопка для запуска предсказания
-if st.button("Predict"):
-item = Item(text=text_input)
-result = predict(item)
-
-# Отображение результата
-st.write("Sentiment:", result[0]['label'])
-st.write("Confidence Score:", result[0]['score'])
+    # Кнопка для запуска предсказания
+    if st.button("Predict"):
+        item = Item(text=text_input)
+        result = predict(item)
+        
+        # Отображение результата
+        st.write("Sentiment:", result[0]['label'])
+        st.write("Confidence Score:", result[0]['score'])
 
 if __name__ == "__main__":
-main()
+    main()

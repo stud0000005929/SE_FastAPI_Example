9eb0268f0926d03c2a018eb4f6ab07816f9c0efb
Zhukov Dmitry <146352203+stud0000228135@users.noreply.github.com>
2024-02-29 19:48:29 +0500

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,9 +1,9 @@
 from fastapi.testclient import TestClient
-from FastAPI import app
+from main import app
 
 client = TestClient(app)
 
 def test_translate_endpoint():
     response = client.post("/predict", json={"text": "Hello, world!"})
     assert response.status_code == 200
-    assert response.json() == {'item': 'POSITIVE'}
+    assert response.json() == {'label': 'POSITIVE'}
